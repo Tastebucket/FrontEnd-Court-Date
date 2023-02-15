334733f6e5a57bbcebbaa090d2f36a060bb55733
jrh <joshuaharris1103@gmail.com>
2023-02-15 16:06:52 -0500

    Working on Geolocator with Mapbox api

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import CourtsIndex from "./courts/CourtsIndex"
+import Mapping from '../api/map.js'
 
 const Home = (props) => {
-	// const { msgAlert, user } = props
+	const { msgAlert, user } = props
 	console.log('props in home', props)
 
 	return (
 		<>
+			<Mapping />
 			<CourtsIndex msgAlert= { props.msgAlert }/>
 		</>
 	)
