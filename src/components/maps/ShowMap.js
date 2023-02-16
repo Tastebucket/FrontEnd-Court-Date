7cb265c3f17f27b25837b218d6a57677e3d7643e
EljerPenny <elijahjpenny@gmail.com>
2023-02-16 16:55:38 -0500

    Show page has map

--- src/components/maps/ShowMap.js
+++ src/components/maps/ShowMap.js
@@ -1,37 +1,26 @@
 import "mapbox-gl/dist/mapbox-gl.css"
 import Map, { Marker, NavigationControl, Popup, FullscreenControl, GeolocateControl } from "react-map-gl"
 import { useState, useCallback, useEffect } from "react"
-import LoadingScreen from "../components/shared/LoadingScreen"
+import LoadingScreen from "../shared/LoadingScreen"
 
 // import { geocoding } from '@mapbox/mapbox-sdk/services/geocoding'
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder'
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
 // import * as CourtsIndex from "src/components/courts/CourtsIndex.js"
 
-export const Mapping = (props) => {
-const { courts } = props
-console.log('these are the courts', courts)
-const [lng, setLng] = useState(-73.769417)
-const [lat, setLat] = useState(40.742054)
+export const ShowMap = (props) => {
+const { court } = props
+console.log('these are the courts', court)
+const [lng, setLng] = useState(court.longitude)
+const [lat, setLat] = useState(court.latitude)
 
-const handleRetrieve = useCallback(
-    (res) => {
-      console.log('these are res features',res)
-    }
-)
+
 // const geocoder = geocoding({ accessToken: 'pk.eyJ1Ijoiam9zaHVhaGFycmlzMTEwMyIsImEiOiJjbGU1cTQ5OGUwOWJrM3V0YzlhYml3Znk1In0.RWn3-nWxtA6_obodRSjaXg' })
-if (!courts) {
+if (!court) {
     // if no courts loaded yet, display 'loading'
     return <LoadingScreen />
 }
-    const mapMarkers = courts.map(court => {
-        console.log('this is one court longitude', court.longitude)
-        const long = court.longitude.toString()
-        const lat = court.latitude.toString()
-        return(
-            <Marker key={court._id} longitude={long} latitude={lat} 
-            />)
-        })
+
 
 return (
 
@@ -39,16 +28,15 @@ return (
     <Map
     mapboxAccessToken= {process.env.REACT_APP_MAPBOX_TOKEN}
     style={{ marginLeft: "5%", justifyContent: "center", width: "90%", height: "250px", borderRadius: "10px", border: "2px solid blue"}}
-    initialViewState={{ longitude: lng, latitude: lat, zoom: 5 }}
+    initialViewState={{ longitude: lng, latitude: lat, zoom: 12 }}
     mapStyle="mapbox://styles/mapbox/streets-v9"
     >
-    {mapMarkers}
+    <Marker longitude={lng.toString()} latitude={lat.toString()}/>
     <NavigationControl position="bottom-right" />
-    <FullscreenControl />
-    <GeolocateControl trackUserLocation onGeolocate={handleRetrieve} />
+    <GeolocateControl />
     </Map>
 </>
     )
 }
 
-export default Mapping
+export default ShowMap
