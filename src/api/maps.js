b60e43d267227af6fe70c3113556e826d0f35f8b
EljerPenny <elijahjpenny@gmail.com>
2023-02-22 14:41:44 -0500

    Figuring out netlify take 2

--- src/api/maps.js
+++ src/api/maps.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 /// Reverse Geocoding
 export const findLocationName = (long,lat) =>{
-    console.log('this is env',process.env)
+    console.log('this is env token',process.env.REACT_APP_MAPBOX_TOKEN)
+    console.log('this is env api adress', process.env.REACT_APP_REVERSE_GEOCODE)
     return axios(`${process.env.REACT_APP_REVERSE_GEOCODE}/${long},${lat}.json?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`)
 }
